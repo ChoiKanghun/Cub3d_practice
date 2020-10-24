f3ef8ccd6c0ae8decb74776fb9deac2e2ee0d6c8
choikanghun <rkdgns4562@naver.com>
2020-10-24 10:44:31 +0900

    Add: comments

--- cub3d/main2.c
+++ cub3d/main2.c
@@ -84,7 +84,7 @@ int main(int argc, char **argv)
         { 
             // cameraX 는 for문의 x값이 카메라 평면 상에 있을 때의 x좌표.
             double cameraX = (2 * x / double(w)) - 1;
-            // planeX == 0; planeY == 0.66; dirVecX = -1; dirVecY = 0;
+            // cameraPlaneX == 0; cameraPlaneY == 0.66; dirVecX = -1; dirVecY = 0;
             // 광선의 방향은 방향벡터 + 카메라평면 * 배수.
             double rayDirectionX = directionVectorX + cameraPlaneX * cameraX;
             double rayDirectionY = directionVectorY + cameraPlaneY * cameraX;
@@ -166,10 +166,10 @@ int main(int argc, char **argv)
 
             /*
                 DDAgorithm 세팅을 완료했고, 이제 그것을 시작하는 부분.
-                아래 while문은 벽에 부딪힐 때까지 매번 한 칸씩 광선 이동.
+                아래 while문은 `벽에 부딪힐 때까지` 매번 한 칸씩 광선 이동.
                 반복할 때마다 x방향으로 한 칸 또는 y방향으로 한 칸 점프.
                 만약 광선의 방향이 x축 방향과 완전히 일치한다면, x방향으로만 한 칸 점프하면 됨.
-                광선이 점프할 때마다 sideDistX, Y는 deltaDistX, Y가 더해지면서 업데이트됨.
+                광선이 점프할 때마다 <sideDistX, Y>에는 <deltaDistX, Y>가 더해지면서 업데이트됨.
             */
             while (hit == 0)
             {
@@ -177,14 +177,14 @@ int main(int argc, char **argv)
                 if (sideDistX < sideDistY)
                 {
                     sideDistX += deltaDistX;
-                    mapX += stepX;
-                    side = 0;
+                    mapX += stepX; // stepX 는 1, -1 중 하나.
+                    side = 0; // x면에 부딪혔다면 side = 0
                 }
                 else
                 {
                     sideDistY += deltaDistY;
-                    mapY += stepY;
-                    side = 1;
+                    mapY += stepY; // stepY는 1, -1 중 하나.
+                    side = 1; // y면에 부딪혔다면 side = 1
                 }
                 // ray가 벽을 만났는지 확인하는 작업
                 if (worldMap[mapX][mapY] > 0)
@@ -195,17 +195,18 @@ int main(int argc, char **argv)
                 벽을 만나 DDAgorithm이 완료됨.
                 이제 광선의 시작점에서 벽까지의 이동거리를 계산할 차례임.
                 광선의 시작점에서 벽까지의 이동거리는 벽의 높이를 계산하는 데 쓰임.
-                H------------ wall
-                | \          \ 실제거리
-                |  \         | 벽에서 camera plane까지의 거리.
-                -----P------- camera plane
+                H------------ :wall
+                | \          \: 실제거리
+                |  \         |: 벽에서 camera plane까지의 거리.
+                -----P------- :camera plane
                 P를 기준으로 벽까지의 실제거리를 사용한다면 fisheye 부작용이 나타남.
                 따라서 벽으로부터 cameraPlane까지의 거리를 사용해야.
                 이에 대한 자세한 설명은 로데브 설명 참고.
 
                 따라서 아래 if-else문은 fisheye (side)effect를 방지하는 코드.
                 (1 - stepX) / 2는 stepX가 -1이면 1이되고 1이면 0이 된다.(-1 or 0)
-                해당 연산은 rayDirectionX가 < 0 일 때 길이에 1을 더해주기 위한 코드이다.
+                해당 연산은 mapX - playerPositionX가 < 0 일 때, 즉 벽 밖으로 갈 때
+                길이에 1을 더해주기 위한 코드이다.
 
                 수직거리를 계산하는 방법은 이렇다. 
                 만약 광선이 처음으로 부딪힌 면이 x면이면 
@@ -222,14 +223,17 @@ int main(int argc, char **argv)
                 perpWallDist = (mapY - playerPositionY + (1 - stepY) / 2)) / rayDirectionY;
             
 
-            // 스크린에 그릴 line의 높이를 계산합니다.
+            // 스크린에 그릴 line의 높이를 계산.
             int lineHeight = (int)(screenHeight / perpWallDist);
 
             /*
                 이제 계산한 거리를 가지고 화면에 그려야 하는 선의 높이를 구할 수 있다.
                 벽을 더 높게 그리거나 낮게 그리고 싶으면 2 * lineHeight 같은 값을 넣을 수도 있다.
-                위에서 구한 lineHeight로부터 우리는 선을 그릴 위치의 시작점과 끝점을 구해낼 수 잇다.
-                만약에 벽의 범위 (0 ~ screenHeight)를 벗어나는 경우 각각 0과 screenHeight - 1을 대입한다.
+                위에서 구한 lineHeight로부터 우리는 선을 그릴 위치의 시작점과 끝점을 구해낼 수 있다.
+                만약에 벽의 범위 (0 ~ screenHeight)를 벗어나는 경우 
+                각각 0과 screenHeight - 1을 대입한다.
+                    +) drawStart와 End에 2로 나눈 값들을 더하는 이유는
+                       screenHeight보다 drawEnd가 커지면 될까 안될까를 생각해보면 알 수 있다.
             */
             int drawStart = (-lineHeight / 2) + (screenHeight / 2);
             if (drawStart < 0)
@@ -239,14 +243,17 @@ int main(int argc, char **argv)
                 drawEnd = screenHeight - 1;
             
             int color;
-            switch (worldMap[mapX][mapY])
-            {
-                case 1: color = 0xFF0000; break;
-                case 2: color = 0x00FF00; break;
-                case 3: color = 0x0000FF; break;
-                case 4: color = 0xFFFFFF; break;
-                default: color = 0x123145; break;
-            }
+            if (worldMap[mapX][mapY] == 1)
+                color = 0xFF0000;
+            else if (worldMap[mapX][mapY] == 2)
+                color = 0x00FF00;
+            else if (worldMap[mapX][mapY] == 3)
+                color = 0x0000FF;
+            else if (worldMap[mapX][mapY] == 4)
+                color = 0xFFFFFF;
+            else
+                color = 0x123145;
+            
 
             // x, y side의 밝기를 달리해줌.
             if (side == 1)
